e566a44a3bd1f4a194af80cd716713cec659ab81
yumaon <110388364+yumaon@users.noreply.github.com>
2023-03-27 00:25:41 +0000

    Ruby 学習

---
+++ paiza_learn_b3.rb
@@ -0,0 +1,137 @@
+h, w = gets.split.map(&:to_i)
+
+h_w_array = []
+h.times do
+  h_w_array << gets.split("").map(&:chomp).reject(&:empty?)
+end
+
+num = gets.split.map(&:to_i)
+x = num[0] - 1
+y = num[1] - 1
+
+move_count = gets.to_i
+
+command_array = []
+move_count.times do
+  command_array << gets.chomp
+end
+
+n = 0
+move_count.times do
+  if command_array[n] == "U"
+    y -= 1
+    while h_w_array[y][x] == "#" do
+      if y == 0
+        break
+      end
+      y -= 1
+    end
+    n += 1
+
+  elsif command_array[n] == "R"
+    x += 1
+    while h_w_array[y][x] == "#" do
+      if x == w - 1
+        break
+      end
+      x += 1
+    end
+    n += 1
+
+  elsif command_array[n] == "D"
+    y += 1
+    while h_w_array[y][x] == "#" do
+      if y == h - 1
+        break
+      end
+      y += 1
+    end
+    n += 1
+
+  elsif command_array[n] == "L"
+    x -= 1
+    while h_w_array[y][x] == "#" do
+      if x == 0
+        break
+      end
+      x -= 1
+    end
+    n += 1
+
+  end
+end
+
+puts [x + 1, y + 1].join(" ")
+
+# h, w = gets.split.map(&:to_i)
+
+# h_w_array = []
+# h.times do
+#   h_w_array << gets.split("").map(&:chomp).reject(&:empty?)
+# end
+
+# num = gets.split.map(&:to_i)
+# x = num[0] - 1
+# y = num[1] - 1
+
+# move_count = gets.to_i
+
+# command_array = []
+# move_count.times do
+#   command_array << gets.chomp
+# end
+
+# n = 0
+# move_count.times do
+#   if command_array[n] == "U"
+#     y -= 1
+#     if h_w_array[y][x] == "#"
+#       while h_w_array[y][x] == "#" do
+#         if y == 0
+#           break
+#         end
+#         y -= 1
+#       end
+#     end
+#     n += 1
+
+#   elsif command_array[n] == "R"
+#     x += 1
+#     if h_w_array[y][x] == "#"
+#       while h_w_array[y][x] == "#" do
+#         if x == w - 1
+#           break
+#         end
+#         x += 1
+#       end
+#     end
+#     n += 1
+
+#   elsif command_array[n] == "D"
+#     y += 1
+#     if h_w_array[y][x] == "#"
+#       while h_w_array[y][x] == "#" do
+#         if y == h - 1
+#           break
+#         end
+#         y += 1
+#       end
+#     end
+#     n += 1
+
+#   elsif command_array[n] == "L"
+#     x -= 1
+#     if h_w_array[y][x] == "#"
+#       while h_w_array[y][x] == "#" do
+#         if x == 0
+#           break
+#         end
+#         x -= 1
+#       end
+#     end
+#     n += 1
+
+#   end
+# end
+
+# puts [x + 1, y + 1].join(" ")
