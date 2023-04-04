12e49a81bfc31c48e3c8ec8b3f6180e23947c669
yumaon <110388364+yumaon@users.noreply.github.com>
2023-04-04 05:34:11 +0000

    Ruby 学習

---
+++ paiza_learn_c31.rb
@@ -0,0 +1,32 @@
+a_t, count = gets.split.map(&:to_i)
+active_time = [*1..a_t]
+
+mission_array = []
+key_time = []
+count.times do
+  time, mission = gets.split.map(&:chomp)
+  key_time << time.to_i
+  mission_array << mission
+end
+
+n = 0
+puts_array = []
+active_time.each do |active|
+  while n < count do
+    if active % key_time[n] == 0
+      if puts_array.any?(active)
+          puts_array = []
+      end
+      puts_array << mission_array[n]
+      n += 1
+    else
+      unless puts_array.any?
+        puts_array << active
+      end
+      n += 1
+    end
+  end
+  puts puts_array.join(" ")
+  n = 0
+  puts_array = []
+end
