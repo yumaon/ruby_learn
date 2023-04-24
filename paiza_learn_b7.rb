b9d7c1d8714fbfa3059285f6fa10ce7b7e97e772
yumaon <110388364+yumaon@users.noreply.github.com>
2023-04-24 01:45:21 +0000

    Ruby 学習 B

--- paiza_learn_b7.rb
+++ paiza_learn_b7.rb
@@ -24,3 +24,32 @@ end
 
 keys = car_hash.keys.sort.last
 puts car_hash[keys]
+
+# 少し修正↓
+
+car_count = gets.to_i
+
+car_hash = {}
+car_ary = []
+car_count.times do
+  car_num = gets.to_i
+  car_ary << car_num
+  car_hash[car_num] = 0
+end
+
+car_ary_sort = car_ary.sort
+
+while car_count != 0 do
+  if car_ary[0] != car_ary_sort[0]
+    car_hash[car_ary[0]] += 1
+    car_top = car_ary.shift
+    car_ary = car_ary.insert(-1, car_top)
+  else
+    car_ary_sort.shift
+    car_ary.shift
+    car_count -= 1
+  end
+end
+
+keys = car_hash.keys.sort.last
+puts car_hash[keys]
